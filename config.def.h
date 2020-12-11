bc87c5c2a8dc04cb9ecfa1c7114507743fa78fc8
Heiko Schmidt <mail@heikoschmidt.info>
2020-12-11 17:58:40 +0100

    Further basic configuration
    - added correct terminal command
    - added functionality for brightness up/down keys

--- config.def.h
+++ config.def.h
@@ -5,7 +5,10 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { 
+	"FontAwesome:size=12",
+	"monospace:size=10"
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -43,6 +46,10 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* function keys */
+static const char *inclight[] = { "xbacklight", "-inc", "10", NULL };
+static const char *declight[] = { "xbacklight", "-dec", "10", NULL };
+
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
@@ -57,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -95,6 +102,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ 0,				XF86XK_MonBrightnessUp,		spawn,	{.v = inclight} },
+	{ 0,				XF86XK_MonBrightnessDown,	spawn,	{.v = declight} },
 };
 
 /* button definitions */
